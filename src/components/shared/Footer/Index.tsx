e23caccc2b81fc9c499042fd68cef0a912d82855
Stack Soumik. <110479389+Soum-ik@users.noreply.github.com>
2025-01-17 14:46:49 +0600

    Update Index.tsx

--- src/components/shared/Footer/Index.tsx
+++ src/components/shared/Footer/Index.tsx
@@ -12,13 +12,13 @@ const Index: React.FC = () => {
 
       <div className="h-auto w-full   bg-bg_dark_blue">
         <div className="mx-auto  max-w-[1920px]">
-          <div className="  pb-[50px]   sm:pb-[75px]   md:pb-[100px] custom_padding xl:px-[110px] xl:pb-[70px] 2xl:px-[170px] 2xl:pb-[125px] 3xl:px-[310px]">
+          <div className="  pb-[50px]   sm:pb-[75px]   md:pb-[100px] custom_padding xl:px-[110px] xl:pb-[70px] 2xl:px-[170px] 2xl:pb-[125px] 3xl:px-[280px]">
             {/* animation */}
             <div className="relative flex flex-col items-center justify-center py-[120px]">
               <h1 className="footer_title z-[300] font-montserrat text-[36px] xl:text-[33px] 2xl:text-[40px] 2.5xl:text-[52px]">
-                Get Up to 15% Off — Show Early <br /> Interest Before We Launch!
+                Revolutionize Your Fleet With Modern, Hardware- Free solution at your fingertips!
               </h1>
-              <CustomeButton href="/collections/select-country" text="Get my discount" className=" capitalize z-[110] mt-[32px]" />
+              <CustomeButton href="/collections/select-country" text="Get early Access" className=" capitalize z-[110] mt-[32px]" />
 
 
               <div className="absolute lg2:-top-[50px] z-[90] lg2:size-[450px] size-[300px] xxxs:size-[340px] rounded-full bg-p_light_blue opacity-20 lg2:blur-[210px] blur-[123px] 2xl:size-[600px]"></div>
@@ -27,7 +27,7 @@ const Index: React.FC = () => {
             <div className=" z-[300] flex items-center  justify-center flex-col gap-[10px]">
               <div className="flex z-[300] cursor-pointer items-center gap-[10px] text-bg_white">
                 <a target="_blank" href="https://www.linkedin.com/company/105785475">
-                  <Image className  =" size-[24px]" src={linkdin} alt="linkdin" /></a>
+                  <Image className=" size-[24px]" src={linkdin} alt="linkdin" /></a>
               </div>
               <h5 className="footer_text font-semibold">©2024 Fleetblox</h5>
             </div>
