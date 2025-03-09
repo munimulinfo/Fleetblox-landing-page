77fb4244aedbd3b53de604b2118f402f54f97376
Sobuz-Mia <sobuz.info0298@gmail.com>
2025-03-09 16:39:02 +0600

    resolve the all animations issue

--- src/components/modules/home/fleetBloxVersus/VersusCard.tsx
+++ src/components/modules/home/fleetBloxVersus/VersusCard.tsx
@@ -1,61 +1,82 @@
 import CheckIcon from "@/components/icons/CheckIcon";
 import CrossIcon from "@/components/icons/CrossIcon";
 import FleetbloxIcon from "@/components/icons/FleetbloxIcon";
-import { motion, MotionStyle } from "framer-motion";
-interface CardProps {
-  number: number;
-  title: string;
-  fleetbloxText: string;
-  traditionalText: string;
-  style: MotionStyle;
-}
-const VersusCard: React.FC<CardProps> = ({
+import { TVersesCard } from "@/types/types";
+import { useTransform, motion } from "framer-motion";
+
+const VirtualCard: React.FC<TVersesCard> = ({
+  index,
   number,
   title,
-  fleetbloxText,
-  traditionalText,
-  style,
+  traditionDes,
+  fleetBloxDes,
+  targetScale,
+  progress,
+  range,
 }) => {
+  const scale = useTransform(progress, range, [1, targetScale]);
+
   return (
-    <motion.div
-      style={style}
-      transition={{ duration: 1.5, ease: [0.25, 0.1, 0.25, 1] }}
-      className="w-[500px] h-[500px] bg-[#fff] z-50 rounded-[16px] shadow-md p-8 flex flex-col gap-[10px] absolute top-0 left-0"
+    <div
+      key={index}
+      className="sticky z-50 top-[0vh] flex h-[100vh] flex-col items-center justify-center xl:top-[13vh] 2xl:top-[12vh] 2.5xl:top-[15vh] 3xl:top-[8vh] "
     >
-      {/* Card Header */}
-      <div className="flex flex-col items-center mb-8">
-        <div className="w-8 h-8 rounded-full border-2 border-[#0336BC] flex items-center justify-center mb-3">
-          <span className="text-[#0336BC] font-semibold">{number}</span>
-        </div>
-        <h3 className="text-[#0336BC] text-xl font-bold">{title}</h3>
-      </div>
+      <motion.div
+        className={`relative z-[200] top-[0px] mx-auto transform flex-col gap-x-[30px] max-h-[500px] h-full lg:h-[500px] max-w-[500px] w-full lg:w-[500px]`}
+        style={{ top: `calc(-5vh + ${index * 15}px)`, scale: scale }}
+      >
+        {index === 0 && (
+          <div
+            className={`absolute left-1/2 top-1/2 mt-[90px] size-[664px] -translate-x-1/2 -translate-y-1/2 rounded-full bg-[#ffe1bf] opacity-40 blur-[200px]`}
+          ></div>
+        )}
+        <div className="hidden md:block lg:ml-10 xl:ml-20 absolute top-[0vh] xl:-top-[8vh] z-[200] w-full h-full">
+          {/* <div className="w-[500px] h-[500px] bg-[#FFE1BF] opacity-30 blur-[100px] rounded-full"></div>
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></div> */}
+          <div className="w-[500px] h-[500px] bg-white rounded-[24px] pt-8 px-4 pb-4 flex flex-col gap-[10px]">
+            {/* Card Header */}
+            <div className="flex flex-col items-center mb-8">
+              <div className="w-8 h-8 rounded-full border-2 border-[#0336BC] flex items-center justify-center mb-3">
+                <span className="text-[#0336BC] font-semibold">{number}</span>
+              </div>
+              <h3 className="text-[#0336BC] text-xl font-bold">{title}</h3>
+            </div>
+            {/* Fleetblox Advantage */}
+            <div className="bg-[#FEFAF0] p-5 rounded-xl flex items-center gap-4">
+              <div className="w-8 h-[29px]">
+                <CheckIcon />
+              </div>
+              <div>
+                <FleetbloxIcon />
+                <p className="text-[16px] font-openSans text-[#4A4A4A]">
+                  {fleetBloxDes}
+                </p>
+              </div>
+            </div>
 
-      {/* Fleetblox Advantage */}
-      <div className="bg-[#FEFAF0] p-5 rounded-xl flex items-center gap-4">
-        <CheckIcon />
-        <div>
-          <FleetbloxIcon />
-
-          <p className="text-[16px] font-openSans text-[#4A4A4A]">
-            {fleetbloxText}
-          </p>
-        </div>
-      </div>
-
-      {/* Traditional Solution */}
-      <div className="bg-[#F7F7F7] p-5 rounded-xl flex items-center justify-center gap-4">
-        <CrossIcon />
-        <div>
-          <div className="text-[#333333] font-semibold mb-1">
-            Traditional Solutions
+            {/* Traditional Solution */}
+            <div className="bg-[#F7F7F7] p-5 rounded-xl flex items-center justify-start gap-4">
+              <div className="w-8 h-[29px]">
+                <CrossIcon />
+              </div>
+              <div>
+                <div className="text-[#333333] font-semibold mb-1">
+                  Traditional Solutions
+                </div>
+                <p className="text-[16px] text-[#4A4A4A] font-openSans">
+                  {traditionDes}
+                </p>
+              </div>
+            </div>
           </div>
-          <p className="text-[16px] text-[#4A4A4A] font-openSans">
-            {traditionalText}
-          </p>
         </div>
-      </div>
-    </motion.div>
+        {/* lg:left-10 xl:left-20 */}
+        {/* right side card end */}
+        <div className="absolute top-[0vh] xl:-top-[8vh] z-[20] rounded-[2px] bg-black opacity-[0.07] blur-[20px] h-[530px] w-[435px] lg:left-[70px] xl:left-[110px]"></div>
+        {/* <div className="absolute bottom-[10px] left-[60px] z-[20] rounded-[15px] bg-black opacity-[0.07] blur-[20px] h-[530px] w-[420px]"></div> */}
+      </motion.div>
+    </div>
   );
 };
 
-export default VersusCard;
+export default VirtualCard;
