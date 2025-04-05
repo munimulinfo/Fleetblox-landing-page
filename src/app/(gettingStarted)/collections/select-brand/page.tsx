083ebfd2d4a637a541f0b5914ed51027e63dbe0d
MD Sajjad Hossen <sajjadmugdho@yahoo.com>
2025-04-05 12:49:49 +0600

    [Done] fix all build error

--- src/app/(gettingStarted)/collections/select-brand/page.tsx
+++ src/app/(gettingStarted)/collections/select-brand/page.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 import BrandSelector from "../../components/select-brand";
+import { Suspense } from "react";
+import Loader from "../../components/Loader";
 
 const selectBrand = () => {
-  return <BrandSelector />;
+  return (
+    <Suspense
+      fallback={
+        <div className="flex items-center justify-center h-screen">
+          <Loader />
+        </div>
+      }
+    >
+      <BrandSelector />
+    </Suspense>
+  );
 };
 
 export default selectBrand;
