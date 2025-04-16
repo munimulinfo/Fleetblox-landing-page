14eebfc8e7a70fd1eba0615cca1c767e9de744c9
Sobuz-Mia <sobuz.info0298@gmail.com>
2025-04-16 14:54:40 +0600

    completed responsive view

--- src/app/(subPage)/solutions/cloud-telematic/page.tsx
+++ src/app/(subPage)/solutions/cloud-telematic/page.tsx
@@ -3,6 +3,7 @@ import TickIcon from "@/components/icons/TickIcon";
 import FeatureCard from "@/components/ui/FeatureCard";
 import { WhyCloudData } from "@/Static_data/solution";
 import Image from "next/image";
+import Link from "next/link";
 
 const page = () => {
   const items = [
@@ -40,28 +41,41 @@ const page = () => {
   return (
     <div>
       {/* hero section */}
-      <section className="bg-[#FAFAFF] pt-[140px] pb-[10px]">
-        <div className="lg:max-w-[calc(100vw-30px)] xxl:max-w-[1440px] mx-auto w-full flex items-center justify-between">
-          <div className="pl-[130px]">
+      <section className="bg-[#FAFAFF] pt-[100px] lg:pt-[140px] pb-[30px] ">
+        <div className="lg:max-w-[calc(100vw-30px)] xxl:max-w-[1440px] mx-auto w-full flex flex-col px-5 lg:flex-row items-center justify-between">
+          <div className="lg:pl-[130px]">
             <h3 className="text-[#0336BC] font-openSans font-bold text-[22px]">
               Cloud Telematics
             </h3>
-            <h1 className="text-[#04082C] text-[52px] font-bold leading-[1.1]">
+            <h1 className="text-[#04082C] text-[36px] lg:text-[52px] font-bold leading-[1.1]">
               The Future of Fleet Management at Your Fingertips
             </h1>
-            <p className="text-[#333] text-[16px] leading-6 mt-4 mb-5 font-openSans">
+            <p className="text-[#333] text-[16px] leading-6 mt-[10px] lg:mt-4 mb-5 font-openSans">
               Experience smarter fleet management with Fleetblox Cloud
               Telematics. Connect and control your fleet from anywhere in just a
               few clicks with our seamless, cloud-based solution.
             </p>
-            <button className="transition-all bg-[#2D65F2] hover:bg-[#0336BC] font-openSans text-white-primary text-white duration-300 hover:w-[144.16px] w-[122.16px] flex items-center px-4 py-3 text-base font-bold rounded-md group">
-              <div className="z-20 whitespace-nowrap">Start today</div>
-              <div className="z-10 transform transition-transform opacity-0 group-hover:opacity-100 -translate-x-4 duration-300 group-hover:translate-x-0">
-                <RightArrowIcon />
-              </div>
-            </button>
+            <Link
+              aria-label="Get started with FleetBlox"
+              href="/getting-started"
+            >
+              <button className="hidden transition-all bg-[#2D65F2] hover:bg-[#0336BC] font-openSans text-white-primary text-white duration-300 hover:w-[144.16px] w-[122.16px] lg:flex items-center px-4 py-3 text-base font-bold rounded-md group">
+                <div className="z-20 whitespace-nowrap">Start today</div>
+                <div className="z-10 transform transition-transform opacity-0 group-hover:opacity-100 -translate-x-4 duration-300 group-hover:translate-x-0">
+                  <RightArrowIcon />
+                </div>
+              </button>
+            </Link>
+            <Link
+              aria-label="Get started with FleetBlox"
+              href="/getting-started"
+            >
+              <button className="lg:hidden bg-[#2D65F2] hover:bg-[#0336BC] text-white w-full flex px-4 py-3 text-[14px] font-openSans font-bold rounded-md justify-center">
+                Switch Today
+              </button>
+            </Link>
           </div>
-          <div className=" w-full py-10 pl-[60px]">
+          <div className=" w-full py-10 lg:pl-[60px]">
             <Image
               src="/images/coludTalematics-hero.png"
               alt="AI assistant for fleet optimization"
@@ -73,9 +87,9 @@ const page = () => {
         </div>
       </section>
       {/* hero section end */}
-      <section className="max-w-[1200px] mx-auto w-full mt-[100px]">
+      <section className="max-w-[1200px] mx-auto w-full mt-[60px] lg:mt-[100px] px-5">
         <div className="max-w-[840px] mx-auto w-full text-center">
-          <h1 className="text-[#04082C] text-[36px] font-bold text-center leading-[1.1] mb-[10px]">
+          <h1 className="text-[#04082C] text-[28px] lg:text-[36px] font-bold text-center leading-[1.1] mb-[10px]">
             How Cloud Telematics Sets Us Apart with Innovative Fleet Solutions
           </h1>
           <p className="text-[#333] text-[16px] leading-6 font-openSans">
@@ -83,12 +97,12 @@ const page = () => {
             seamless fleet management for unmatched efficiency and control
           </p>
         </div>
-        <div className="flex items-center py-[60px] justify-between">
+        <div className="flex flex-col lg:flex-row items-center py-8 lg:py-[60px] justify-between">
           <div className="py-10">
             <Image
               src="/images/howTelamaticsSet.png"
-              alt="AI assistant for fleet optimization"
-              width={520}
+              alt="how telematics set us apart"
+              width={600}
               height={387}
               className="object-contain w-full"
             />
@@ -114,29 +128,40 @@ const page = () => {
         </div>
       </section>
       {/* why Cloud section */}
-      <section className="max-w-[1200px] mx-auto w-full my-[100px]">
-        <h1 className="mb-[60px] text-center text-[#04082C] text-[36px] font-bold">
+      <section className="max-w-[1200px] mx-auto w-full my-10 lg:my-[100px] px-5">
+        <h1 className="mb-[40px] lg:mb-[60px] text-center text-[#04082C] text-[28px] lg:text-[36px] font-bold">
           Why Cloud Telematics is the Future
         </h1>
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-[30px]">
+        <div className="lg:grid hidden grid-cols-1 lg:grid-cols-3 justify-items-center gap-[30px]">
           {WhyCloudData?.slice(0, 3).map((data, index) => (
             <FeatureCard
               key={index}
               icon={<data.icon />}
               title={data.title}
               description={data.description}
-              className="w-[380px]"
+              className="max-w-[380px] w-full"
             />
           ))}
         </div>
-        <div className="flex justify-center mt-[30px] gap-[30px]">
+        <div className="grid lg:hidden grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center lg:gap-[30px] gap-[20px]">
+          {WhyCloudData?.map((data, index) => (
+            <FeatureCard
+              key={index}
+              icon={<data.icon />}
+              title={data.title}
+              description={data.description}
+              // className="w-full"
+            />
+          ))}
+        </div>
+        <div className="lg:flex hidden flex-col items-center justify-items-center justify-center lg:flex-row mt-[30px] gap-[30px]">
           {WhyCloudData?.slice(3, 5).map((data, index) => (
             <FeatureCard
               key={index}
               icon={<data.icon />}
               title={data.title}
               description={data.description}
-              className="w-[380px]"
+              className="max-w-[380px] w-full"
             />
           ))}
         </div>
