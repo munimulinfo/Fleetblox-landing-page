de0f37dcbf2a53401bda2d91e45f8d9b20730a6d
Sobuz-Mia <sobuz.info0298@gmail.com>
2025-04-26 20:49:03 +0600

    setup feature page and routing

---
+++ src/app/(subPage)/features/fleet-connectivity/page.tsx
@@ -0,0 +1,57 @@
+import RightArrowIcon from "@/components/icons/RightArrowIcon";
+import Image from "next/image";
+import Link from "next/link";
+
+const page = () => {
+  return (
+    <div>
+      <section className="bg-[#FAFAFF] pt-[100px] lg:pt-[140px] pb-[30px] ">
+        <div className="lg:max-w-[calc(100vw-30px)] xxl:max-w-[1440px] mx-auto w-full flex flex-col px-5 lg:flex-row items-center justify-between">
+          <div className="lg:pl-[130px]">
+            <h3 className="text-[#0336BC] font-openSans font-bold text-[22px]">
+              Fleet Connectivity
+            </h3>
+            <h1 className="text-[#04082C] text-[36px] lg:text-[52px] font-bold leading-[1.1]">
+              Smarter, Hardware-Free Vehicle Control in Real Time
+            </h1>
+            <p className="text-[#333] text-[16px] leading-6 mt-[10px] lg:mt-4 mb-5 font-openSans">
+              Fleetblox’s real-time cloud connectivity delivers complete,
+              real-time visibility and control over your fleet — without the
+              need for additional hardware.
+            </p>
+            <Link
+              aria-label="Get started with FleetBlox"
+              href="/getting-started"
+            >
+              <button className="hidden transition-all bg-[#2D65F2] hover:bg-[#0336BC] font-openSans text-white-primary text-white duration-300 hover:w-[144.16px] w-[122.16px] lg:flex items-center px-4 py-3 text-base font-bold rounded-md group">
+                <div className="z-20 whitespace-nowrap">Start today</div>
+                <div className="z-10 transform transition-transform opacity-0 group-hover:opacity-100 -translate-x-4 duration-300 group-hover:translate-x-0">
+                  <RightArrowIcon />
+                </div>
+              </button>
+            </Link>
+            <Link
+              aria-label="Get started with FleetBlox"
+              href="/getting-started"
+            >
+              <button className="lg:hidden bg-[#2D65F2] hover:bg-[#0336BC] text-white w-full flex px-4 py-3 text-[14px] font-openSans font-bold rounded-md justify-center">
+                Start today
+              </button>
+            </Link>
+          </div>
+          <div className=" w-full py-10 lg:pl-[60px]">
+            <Image
+              src="/images/fleet-connectivity-hero.png"
+              alt="Remote Scalability Hero"
+              width={700}
+              height={491}
+              className="object-contain w-full"
+            />
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default page;
