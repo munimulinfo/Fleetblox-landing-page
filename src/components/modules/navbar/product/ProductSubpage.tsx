c18273181a022781d8de31758b7bbf3a7f7fe1ef
Sobuz-Mia <sobuz.info0298@gmail.com>
2025-05-03 19:53:51 +0600

    linked up with header and footer with maintenance page

--- src/components/modules/navbar/product/ProductSubpage.tsx
+++ src/components/modules/navbar/product/ProductSubpage.tsx
@@ -37,7 +37,7 @@ const ProductSubpage = () => {
               </button>
             </h1>
           </Link>
-          <Link href="/under-development">
+          <Link href="/features/dynamic-maintenancet">
             <h1 className="text-[#333] text-[14px] leading-5 font-openSans py-[10px] lg:py-0 font-bold lg:font-semibold hover:text-[#7D7D7D]">
               Maintenance
             </h1>
